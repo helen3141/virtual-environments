f3ba0f5c107831f5e5e97b24e000ca5abac44f5a
helen3141 <helchoi@microsoft.com>
2020-10-15 16:17:11 -0700

    replace server command Install-WindowsFeature with client command Enable-WindowsOptionalFeature

--- images/win/scripts/Installers/Initialize-VM.ps1
+++ images/win/scripts/Installers/Initialize-VM.ps1
@@ -49,15 +49,15 @@ else {
 # Install .NET Framework 3.5 (required by Chocolatey)
 # Explicitly install all 4.7 sub features to include ASP.Net.
 # As of  1/16/2019, WinServer 19 lists .Net 4.7 as NET-Framework-45-Features
-Install-WindowsFeature -Name NET-Framework-Features -IncludeAllSubFeature
-Install-WindowsFeature -Name NET-Framework-45-Features -IncludeAllSubFeature
+Enable-WindowsOptionalFeature -Name NET-Framework-Features -IncludeAllSubFeature
+Enable-WindowsOptionalFeature -Name NET-Framework-45-Features -IncludeAllSubFeature
 if (Test-IsWin16) {
-    Install-WindowsFeature -Name BITS -IncludeAllSubFeature
-    Install-WindowsFeature -Name DSC-Service
+    Enable-WindowsOptionalFeature -Name BITS -IncludeAllSubFeature
+    Enable-WindowsOptionalFeature -Name DSC-Service
 }
 
 # Install FS-iSCSITarget-Server
-$fsResult = Install-WindowsFeature -Name FS-iSCSITarget-Server -IncludeAllSubFeature -IncludeManagementTools
+$fsResult = Enable-WindowsOptionalFeature -Name FS-iSCSITarget-Server -IncludeAllSubFeature -IncludeManagementTools
 if ( $fsResult.Success ) {
     Write-Host "FS-iSCSITarget-Server has been successfully installed"
 } else {
