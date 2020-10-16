76f583d52149cb0aecfa7e46f1aa78ed89c1d43f
helen3141 <helchoi@microsoft.com>
2020-10-15 17:36:25 -0700

    comment out instead

--- images/win/scripts/Installers/Initialize-VM.ps1
+++ images/win/scripts/Installers/Initialize-VM.ps1
@@ -49,21 +49,21 @@ else {
 # Install .NET Framework 3.5 (required by Chocolatey)
 # Explicitly install all 4.7 sub features to include ASP.Net.
 # As of  1/16/2019, WinServer 19 lists .Net 4.7 as NET-Framework-45-Features
-Enable-WindowsOptionalFeature -Name NET-Framework-Features -IncludeAllSubFeature
-Enable-WindowsOptionalFeature -Name NET-Framework-45-Features -IncludeAllSubFeature
-if (Test-IsWin16) {
-    Enable-WindowsOptionalFeature -Name BITS -IncludeAllSubFeature
-    Enable-WindowsOptionalFeature -Name DSC-Service
-}
+# Install-WindowsFeature -Name NET-Framework-Features -IncludeAllSubFeature
+# Install-WindowsFeature -Name NET-Framework-45-Features -IncludeAllSubFeature
+# if (Test-IsWin16) {
+#     Install-WindowsFeature -Name BITS -IncludeAllSubFeature
+#     Install-WindowsFeature -Name DSC-Service
+# }
 
-# Install FS-iSCSITarget-Server
-$fsResult = Enable-WindowsOptionalFeature -Name FS-iSCSITarget-Server -IncludeAllSubFeature -IncludeManagementTools
-if ( $fsResult.Success ) {
-    Write-Host "FS-iSCSITarget-Server has been successfully installed"
-} else {
-    Write-Host "Failed to install FS-iSCSITarget-Server"
-    exit 1
-}
+# # Install FS-iSCSITarget-Server
+# $fsResult = Install-WindowsFeature -Name FS-iSCSITarget-Server -IncludeAllSubFeature -IncludeManagementTools
+# if ( $fsResult.Success ) {
+#     Write-Host "FS-iSCSITarget-Server has been successfully installed"
+# } else {
+#     Write-Host "Failed to install FS-iSCSITarget-Server"
+#     exit 1
+# }
 
 Write-Host "Disable UAC"
 Disable-UserAccessControl
