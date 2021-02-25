351804379b624a0b052761d0c396792e74e688bc
helen3141 <helchoi@microsoft.com>
2021-02-24 16:41:35 -0800

    pwsh core tls

--- images/win/scripts/Installers/Install-PowershellCore.ps1
+++ images/win/scripts/Installers/Install-PowershellCore.ps1
@@ -2,7 +2,8 @@
 ##  File:  Install-PowershellCore.ps1
 ##  Desc:  Install PowerShell Core
 ################################################################################
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor "Tls12"
 
 Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI -Quiet"
-
+[System.Environment]::SetEnvironmentVariable("POWERSHELL_UPDATECHECK", "Off", [System.EnvironmentVariableTarget]::Machine)
 Invoke-PesterTests -TestFile "Tools" -TestName "PowerShell Core"
